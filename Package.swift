c81d0e58b01e8ad20bd7ebbaf4d2eb41430e2ee1
noppefoxwolf <noppelabs@gmail.com>
2019-10-26 19:35:52 +0900

    update

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "BitriseSwift",
+    platforms: [
+        .iOS(SupportedPlatform.IOSVersion.v13),
+        .macOS(SupportedPlatform.MacOSVersion.v10_15),
+        .tvOS(SupportedPlatform.TVOSVersion.v13),
+        .watchOS(SupportedPlatform.WatchOSVersion.v6),
+    ],
     products: [
         .library(name: "BitriseSwift", targets: ["BitriseSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("4.8.2")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
     ],
     targets: [
         .target(name: "BitriseSwift", dependencies: [
